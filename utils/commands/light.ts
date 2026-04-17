6415d938a63a602f2c5e8249e8989a2a70e0b542
DarkLocuts <darklocuts23@gmail.com>
2026-04-17 20:55:38 +0700

    update: docker

--- utils/commands/light.ts
+++ utils/commands/light.ts
@@ -6,7 +6,6 @@ import { makeMigrationCommand } from "./make/basic-migration";
 import { makeControllerCommand } from "./make/basic-controller";
 import { makeLightModelCommand } from "./make/light-model";
 import { makeLightControllerCommand } from "./make/light-controller";
-import { makeDaMigrationCommand } from "./make/da-migration";
 import { makeQueueCommand } from "./make/queue";
 import { makeMailCommand } from "./make/mail";
 import { makeBlueprintCommand } from "./make/blueprint";
@@ -14,7 +13,6 @@ import { makeNotificationCommand } from "./make/notification";
 
 import { migrateCommand, migrateFreshCommand } from "./runner/migration";
 import { seederCommand } from "./runner/seeder";
-import { daMigrateCommand, daMigrateFreshCommand } from "./runner/da-migration";
 import { blueprintCommand } from "./runner/blueprint/runner";
 
 
@@ -29,7 +27,6 @@ program.addCommand(makeControllerCommand);
 program.addCommand(makeModelCommand);
 program.addCommand(makeMigrationCommand);
 program.addCommand(makeSeederCommand);
-program.addCommand(makeDaMigrationCommand);
 program.addCommand(makeLightControllerCommand);
 program.addCommand(makeLightModelCommand);
 program.addCommand(makeQueueCommand);
@@ -41,7 +38,5 @@ program.addCommand(migrateCommand);
 program.addCommand(migrateFreshCommand);
 program.addCommand(seederCommand);
 program.addCommand(blueprintCommand);
-program.addCommand(daMigrateCommand);
-program.addCommand(daMigrateFreshCommand);
 
 program.parse(process.argv);
