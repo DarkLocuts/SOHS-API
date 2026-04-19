f6a7f8c599a31c18cf5a4368bb57825e29200fb8
DarkLocuts <darklocuts23@gmail.com>
2026-04-19 09:33:14 +0700

    fix: middleware cors

--- utils/middleware.util.ts
+++ utils/middleware.util.ts
@@ -77,32 +77,37 @@ export const middleware = {
   // ## Middleware: Cors handler
   // =============================>
   Cors: (app: Elysia) => app.onRequest(({ request, set }) => {
-      // const origin                       = request.headers.get('origin') ?? ''
-      let allowedOrigin: string          = '*'
+      const origin         =  request.headers.get('origin') ?? ''
+      let   allowedOrigin  =  ''
+      const originsConf    =  process.env.APP_CORS_ORIGINS || ''
 
-      // const originsConf = process.env.APP_CORS_ORIGINS || '*'
+      if (originsConf === '*') {
+        allowedOrigin = origin
+      } else {
+        try {
+          const allowedOrigins = JSON.parse(originsConf)
 
-      // if (originsConf !== '*') {
-      //   try {
-      //     const allowedOrigins = JSON.parse(originsConf)
-      //     if (Array.isArray(allowedOrigins) && allowedOrigins.includes(origin)) {
-      //         allowedOrigin = origin || ""
-      //     }
-      //   } catch (e) {
-      //     const em = 'Cors Error: Failed to parse APP_CORS_ORIGINS, fallback to "*"'
-      //     logger.error(em, { error: em })
-      //     allowedOrigin = ''
-      //   }
-      // }
-      
-      set.headers['Access-Control-Allow-Origin']      = allowedOrigin
-      // set.headers['Access-Control-Allow-Methods']     = process.env.APP_CORS_METHODS || 'GET, POST, PUT, DELETE, OPTIONS'
-      set.headers['Access-Control-Allow-Methods']     = 'GET, POST, PUT, DELETE, OPTIONS'
-      set.headers['Access-Control-Allow-Headers']     = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Option, x-App'
+          if (Array.isArray(allowedOrigins) && allowedOrigins.includes(origin)) {
+            allowedOrigin = origin
+          }
+        } catch (e) {
+          logger.error('Cors Error: Failed to parse APP_CORS_ORIGINS')
+        }
+      }
+
+      if (allowedOrigin) {
+        set.headers['Access-Control-Allow-Origin'] = allowedOrigin
+      }
+
+      set.headers['Access-Control-Allow-Methods'] = process.env.APP_CORS_METHODS || 'GET, POST, PUT, DELETE, OPTIONS'
+      set.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Option, x-App'
       set.headers['Access-Control-Allow-Credentials'] = 'true'
 
       if (request.method === 'OPTIONS') {
-          return new Response(null, { status: 204, })
+        return new Response(null, {
+          status: 204,
+          headers: new Headers(set.headers as any)
+        })
       }
   }),
 
